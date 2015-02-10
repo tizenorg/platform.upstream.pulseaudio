4f36da90c3796228a0c1c97022a5116994ea4ca3
Seonmi Jin <seonmi.jin@samsung.com>
2015-02-10 17:57:13 +0900

    pulse: add to module-tizenaudio-sink
    Change-Id: Id13ebd31770ddc0434120199288b6ba814cd8a7b
    Signed-off-by: Seonmi Jin <seonmi.jin@samsung.com>

--- src/modules/tizen-audio.h
+++ src/modules/tizen-audio.h
@@ -267,6 +267,10 @@ typedef struct audio_interface {
     audio_return_t (*set_route)(void *userdata, uint32_t session, uint32_t subsession, uint32_t device_in, uint32_t device_out, uint32_t route_flag);
     audio_return_t (*alsa_pcm_open)(void *userdata, void **pcm_handle, char *device_name, uint32_t direction, int mode);
     audio_return_t (*alsa_pcm_close)(void *userdata, void *pcm_handle);
+    audio_return_t (*pcm_open)(void *userdata, void **pcm_handle, void *sample_spec, uint32_t direction);
+    audio_return_t (*pcm_close)(void *userdata, void *pcm_handle);
+    audio_return_t (*pcm_avail)(void *pcm_handle);
+    audio_return_t (*pcm_write)(void *pcm_handle, const void *buffer, uint32_t frames);
     audio_return_t (*set_mixer_value_string)(void *userdata, const char* ctl, const char* value);
 
 } audio_interface_t;
@@ -287,6 +291,10 @@ audio_return_t audio_set_mute (void *userdata, audio_info_t *info, uint32_t volu
 audio_return_t audio_set_session (void *userdata, uint32_t session, uint32_t subsession, uint32_t cmd);
 audio_return_t audio_alsa_pcm_open (void *userdata, void **pcm_handle, char *device_name, uint32_t direction, int mode);
 audio_return_t audio_alsa_pcm_close (void *userdata, void *pcm_handle);
+audio_return_t audio_pcm_open(void *userdata, void **pcm_handle, void *sample_spec, uint32_t direction);
+audio_return_t audio_pcm_close (void *userdata, void *pcm_handle);
+audio_return_t audio_pcm_avail(void *pcm_handle);
+audio_return_t audio_pcm_write(void *pcm_handle, const void *buffer, uint32_t frames);
 audio_return_t audio_set_route (void *userdata, uint32_t session, uint32_t subsession, uint32_t device_in, uint32_t device_out, uint32_t route_flag);
 
 #endif

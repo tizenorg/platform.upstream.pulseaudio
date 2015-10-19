f8c9fa13d85594d5350d5d678c8f1b6767666f17
Sangchul Lee <sc11.lee@samsung.com>
2015-10-19 10:44:18 +0900

    hal: modify tizen-audio.h
    [Version] 5.0-46
    [Profile] Common
    [Issue Type] API modification
    
    Change-Id: I4b3168ca4c8f439f1aa767b74e5fb62c54440b2e

--- src/modules/tizen-audio.h
+++ src/modules/tizen-audio.h
@@ -81,10 +81,10 @@ typedef struct audio_interface {
     audio_return_t (*do_route)(void *userdata, audio_route_info_t *info);
     audio_return_t (*update_route_option)(void *userdata, audio_route_option_t *option);
     audio_return_t (*update_stream_connection_info) (void *userdata, audio_stream_info_t *info, uint32_t is_connected);
+    audio_return_t (*get_buffer_attr)(void *userdata, uint32_t direction, const char *latency, uint32_t samplerate, int format, uint32_t channels, uint32_t *maxlength, uint32_t *tlength, uint32_t *prebuf, uint32_t* minreq, uint32_t *fragsize);
+    /* Interface of PCM device */
     audio_return_t (*alsa_pcm_open)(void *userdata, void **pcm_handle, char *device_name, uint32_t direction, int mode);
     audio_return_t (*alsa_pcm_close)(void *userdata, void *pcm_handle);
-
-    /* Interface of PCM device */
     audio_return_t (*pcm_open)(void *userdata, void **pcm_handle, void *sample_spec, uint32_t direction);
     audio_return_t (*pcm_start)(void *userdata, void *pcm_handle);
     audio_return_t (*pcm_stop)(void *userdata, void *pcm_handle);
@@ -92,9 +92,6 @@ typedef struct audio_interface {
     audio_return_t (*pcm_avail)(void *userdata, void *pcm_handle, uint32_t *avail);
     audio_return_t (*pcm_write)(void *userdata, void *pcm_handle, const void *buffer, uint32_t frames);
     audio_return_t (*pcm_read)(void *userdata, void *pcm_handle, void *buffer, uint32_t frames);
-
-    audio_return_t (*get_buffer_attr)(void *userdata, uint32_t direction, const char *latency, uint32_t samplerate, int format, uint32_t channels, uint32_t *maxlength, uint32_t *tlength, uint32_t *prebuf, uint32_t* minreq, uint32_t *fragsize);
-    audio_return_t (*set_route)(void *userdata, uint32_t session, uint32_t subsession, uint32_t device_in, uint32_t device_out, uint32_t route_flag);
 } audio_interface_t;
 
 audio_return_t audio_init (void **userdata, void *platform_data);
@@ -108,6 +105,7 @@ audio_return_t audio_set_volume_mute (void *userdata, audio_volume_info_t *info,
 audio_return_t audio_do_route (void *userdata, audio_route_info_t *info);
 audio_return_t audio_update_route_option (void *userdata, audio_route_option_t *option);
 audio_return_t audio_update_stream_connection_info (void *userdata, audio_stream_info_t *info, uint32_t is_connected);
+audio_return_t audio_get_buffer_attr (void *userdata, uint32_t direction, const char *latency, uint32_t samplerate, int format, uint32_t channels, uint32_t *maxlength, uint32_t *tlength, uint32_t *prebuf, uint32_t* minreq, uint32_t *fragsize);
 audio_return_t audio_alsa_pcm_open (void *userdata, void **pcm_handle, char *device_name, uint32_t direction, int mode);
 audio_return_t audio_alsa_pcm_close (void *userdata, void *pcm_handle);
 audio_return_t audio_pcm_open (void *userdata, void **pcm_handle, void *sample_spec, uint32_t direction);
@@ -117,6 +115,4 @@ audio_return_t audio_pcm_close (void *userdata, void *pcm_handle);
 audio_return_t audio_pcm_avail (void *userdata, void *pcm_handle, uint32_t *avail);
 audio_return_t audio_pcm_write (void *userdata, void *pcm_handle, const void *buffer, uint32_t frames);
 audio_return_t audio_pcm_read (void *userdata, void *pcm_handle, void *buffer, uint32_t frames);
-audio_return_t audio_get_buffer_attr (void *userdata, uint32_t direction, const char *latency, uint32_t samplerate, int format, uint32_t channels, uint32_t *maxlength, uint32_t *tlength, uint32_t *prebuf, uint32_t* minreq, uint32_t *fragsize);
-audio_return_t audio_set_route (void *userdata, uint32_t session, uint32_t subsession, uint32_t device_in, uint32_t device_out, uint32_t route_flag);
 #endif
